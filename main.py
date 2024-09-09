3325a022f29a24853ef6290eb06811a7e8ae249a
SqlFR <florian.lazzarini@gmail.com>
2024-09-08 20:25:22 -0400

    Buton reset done

--- main.py
+++ main.py
@@ -1,75 +1,100 @@
-import tkinter
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 
 
-def reset_grid():
-    pass
-# Inverse la couleur d'une cellule lors du click
-def change_color_cell(event):
-    print(event.widget['style'])
-    if event.widget['style'] == 'CellG.TFrame':
-        event.widget.config(style='CellB.TFrame')
-    else:
-        event.widget.config(style='CellG.TFrame')
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Le jeu de la vie")
+        self.geometry("800x600")
+
+        # Config bouton Quitter
+        self.button_quit = ttk.Button(self, text="Quitter")
+        self.button_quit['command'] = self.destroy
+        self.button_quit.grid(column=1, row=0)
+
+        # Config bouton Reset grille
+        self.button_reset = ttk.Button(self, text="Réinitialiser")
+        self.button_reset['command'] = self.reset_grid
+        self.button_reset.grid(column=1, row=1)
+
+    @staticmethod
+    def reset_grid(self):
+        for cell in Cell.instances:
+            cell.kill_cell()
 
 
-total_cells = []
+class MainFrame(ttk.Frame):
 
-
-class CellAlive(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
-
-        print(self.widgetName)
-        self.style = 'CellB.TFrame'
+        self.grid()
 
 
-# Methode d'une cellule et bind du click mouse1
-def cell():
-    c = ttk.Frame(canvas, width=19, height=19, class_='CellAlive')
-    c.bind("<Button-1>", on_click)
-    total_cells.append(c)
-    return c
+class MainGrid(ttk.Frame):
+
+    def __init__(self, container):
+        size_grid = 500
+        super().__init__(container)
+        self.canvas = Canvas(container, width=size_grid, height=size_grid, background='gray')
+        self.canvas.grid()
+
+        for i in range(0, size_grid, 20):
+            for j in range(0, size_grid, 20):
+                self.canvas.create_rectangle(i, j, (i + 20), (j + 20), width=1)
+                self.canvas.create_window(i + 10, j + 10, window=Cell(self.canvas, is_alive=False))
 
 
-# Methode lors du click mouse1 sur une cellule
-def on_click(event):
-    change_color_cell(event)
+class Cell(ttk.Frame):
+
+    instances = []
+
+    def __init__(self, container, is_alive):
+        super().__init__(container)
+
+        self.instances.append(self)
+        self.is_alive = is_alive
+        self.config(width=19)
+        self.config(height=19)
+        self.bind("<Button-1>", self.on_click)
+        self.update_state()
+
+    def __del__(self):
+        self.instances.remove(self)
+
+    # Configure la cellule selon son état
+    def update_state(self):
+        # Styles de cellule
+        cell_style = ttk.Style()
+        cell_style.configure('CellB.TFrame', background='black')
+        cell_style.configure('CellG.TFrame', background='grey')
+
+        if self.is_alive:
+            self.config(style='CellB.TFrame')
+        else:
+            self.config(style='CellG.TFrame')
+
+    def toggle_state(self):
+        # Change l'état de la cellule
+        self.is_alive = not self.is_alive
+        self.update_state()
+
+    def kill_cell(self):
+        self.is_alive = False
+        self.update_state()
+
+    def live_cell(self):
+        self.is_alive = True
+        self.update_state()
+
+    def on_click(self, event):
+        self.toggle_state()
 
 
-root = Tk()
-root.title('Le jeu de la vie')
-
-# Initialisation de la grille principale
-mainframe = ttk.Frame(root)
-mainframe.grid()
-
-# Bouton Quitter
-btn_quit = ttk.Button(mainframe, text="Quitter", command=root.destroy)
-btn_quit.grid(column=1, row=0)
-
-# Bouton Reset de la grille
-btn_reset = ttk.Button(mainframe, text="Réinitialiser", command=reset_grid)
-btn_reset.grid(column=1, row=1)
-
-# Initialisation du canvas
-canvas = Canvas(mainframe, width=400, height=400, background='gray')
-canvas.grid()
-
-# Style de cellule
-cell_style_grey = ttk.Style()
-cell_style_grey.configure('CellG.TFrame', background='grey')
-cell_style_black = ttk.Style()
-cell_style_black.configure('CellB.TFrame', background='black')
-
-
-# Dessine les lignes (horizontale et verticale) de la grille
-# Instancie les cellules de la grille
-for i in range(0, 400, 20):
-    for j in range(0, 400, 20):
-        canvas.create_rectangle(i, j, (i+20), (j+20), width=1)
-        canvas.create_window(i+10, j+10, window=cell())
-
-
-root.mainloop()
+if __name__ == "__main__":
+    app = App()
+    frame = MainFrame(app)
+    grid = MainGrid(frame)
+    app.mainloop()
